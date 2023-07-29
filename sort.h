23e20508e01a8e3a5f06e1385bb989144e6c891b
Dev <kevguama@gmail.com>
2023-07-29 13:54:58 +0300

    12. DealerDebuging_2.1

--- sort.h
+++ sort.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "sort.h"
 
 /**
  * struct listint_s - Doubly linked list node
@@ -18,6 +19,7 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/* Prototypes for mandatory tasks */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void swap_nodes(listint_t **list, listint_t **node1, listint_t **node2);
@@ -28,4 +30,22 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 listint_t *create_listint(const int *array, size_t size);
 
-#endif
+/* Prototypes for advanced tasks */
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+
+/* Prototypes for print functions */
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
+
+/* Prototypes for helper functions */
+void swap(int *a, int *b);
+void heapify(int *array, int end, int start, size_t size);
+
+#endif /* SORT_H */
